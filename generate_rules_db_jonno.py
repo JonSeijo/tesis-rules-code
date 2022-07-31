dcab1f31bc8a6607ee8a237cc20f8460325a06b6
jonno <jon.seijo@gmail.com>
2022-07-31 14:02:08 +0100

    Insert values in new tables

--- generate_rules_db_jonno.py
+++ generate_rules_db_jonno.py
@@ -2,10 +2,20 @@
 
 import sqlite3
 
+import info_rules
+
 filename_db = "output/dbs/rules_db_jonno.db"
 
+def delete_tables(connection):
+    print("- Deleting old tables...")
+    cursor = connection.cursor()
+    cursor.execute('''DROP TABLE rule_jonno''')
+    cursor.execute('''DROP TABLE rule_metadata''')
+    connection.commit()
+
+
 def create_tables(connection):
-    print("Creating tables...")
+    print("- Creating new tables...")
 
     cursor = connection.cursor()
 
@@ -19,9 +29,9 @@ def create_tables(connection):
         rule TEXT,
         antecedent TEXT,
         consequent TEXT,
-        rule_type INTEGER,
-        rule_type_delta INTEGER,
-        rule_filename TEXT,
+        rule_type TEXT,
+        rule_type_simple TEXT,
+        rule_size INTEGER,
         support REAL,
         confidence REAL,
         lift REAL,
@@ -31,7 +41,7 @@ def create_tables(connection):
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS rule_metadata
         (id_rule_metadata INTEGER PRIMARY KEY,
-        rules_filename TEXT,
+        rules_filename TEXT UNIQUE,
         family TEXT,
         min_support TEXT,
         min_confidence TEXT,
@@ -40,11 +50,99 @@ def create_tables(connection):
 
     connection.commit()
 
+def insert_rule_metadata(connection, family, min_len, 
+        mr_type, min_support, min_confidence, filename_rules):
+    
+    print("- Inserting rules metadata")
+    
+    rule_metadata = (filename_rules, family, 
+        min_support, min_confidence, min_len, mr_type)
+    
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        INSERT INTO rule_metadata( 
+            rules_filename,
+            family,
+            min_support,
+            min_confidence,
+            min_len,
+            maximal_repeat_type)
+        VALUES (?,?,?,?,?,?)''', rule_metadata)
+
+    rule_metadata_id = cursor.lastrowid
+    
+    connection.commit()
+
+    return rule_metadata_id
+
+# Podria haber usado algo como pandas.DataFrame.to_sql? Si
+def insert_rules(connection, rules_df, metadata_id):
+    print("- Inserting rules data")
+    cursor = connection.cursor()
+
+    for index, rr in rules_df.iterrows():
+
+        rule_to_insert = (
+            'TODO_rule',
+            'TODO_antecedent',
+            rr['consequent'],
+            rr['ruletype'],
+            rr['ruletype_simple'],
+            'TODO_rulesize',
+            'TODO_support',
+            'TODO_confidence',
+            'TODO_lift',
+            metadata_id
+        )
+
+        cursor.execute('''
+            INSERT INTO rule_jonno(
+                rule,
+                antecedent,
+                consequent,
+                rule_type,
+                rule_type_simple,
+                rule_size,
+                support,
+                confidence,
+                lift,
+                id_rule_metadata)
+            VALUES (?,?,?,?,?,?,?,?,?,?)''', rule_to_insert)
+
+    connection.commit()
+
+
 if __name__ == '__main__':
+
+    # TODO: Parametrizar
+    # TODO: Permitir pasar directamente un rule path?
+    family = "NEWAnk"
+    min_len = "len4"
+    mr_type = "ALL_sub"
+    min_support = "0.025"
+    min_confidence = "0.9"
+
+    filename_rules = f"output/rules/{family}_{min_len}_{mr_type}_s{min_support}_c{min_confidence}.csv"
+    # filename_rules = "output/rules/NEWAnk_len4_ALL_sub_s0.025_c0.9.csv"
+
+    print("\n\n\n\n")
+    print("filename_rules:", filename_rules)
+
 
     print("Connecting to db:", filename_db)
     connection = sqlite3.connect(filename_db)
 
+    # TODO: parametrizar flush
+    delete_tables(connection)
+
     create_tables(connection)
+
+    rules_df = info_rules.build_df_rules_from_path(filename_rules)
+    
+    metadata_id = insert_rule_metadata(connection, family, min_len, 
+        mr_type, min_support, min_confidence, filename_rules)
+
+    insert_rules(connection, rules_df, metadata_id)
 
     connection.close()
