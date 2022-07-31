13461f7a58f64f505ebab88bf0ee6267771f2c03
jonno <jon.seijo@gmail.com>
2022-07-31 13:18:20 +0100

    parameters

--- info_rules.py
+++ info_rules.py
@@ -73,20 +73,42 @@ def build_itemset(s: str) -> Set[str]:
 class RuleBuildException(Exception):
     pass
 
-def get_ruletypes(rules_str: List[str]) -> Dict[str, str]:
-    ruletypes = {}
-    for rule_str in rules_str:
-        rule = rg.Rule(rule_str)
+def get_ruletypes(rules: List[rg.Rule]) -> List[str]:
+    ruletypes = []
+    for rule in rules:
         rtype = rule.getRuleTypeText()
-        ruletypes[rule_str] = rtype
+        ruletypes.append(rtype)
     return ruletypes
 
-def ruletypes_with(substr: str, ruletypes_by_rule: Dict[str, str]) -> List[str]:
+def ruletypes_with(substr: str, rules: List[str], ruletypes: List[str]) -> List[str]:
     matching = []
-    for rule, rtype in ruletypes_by_rule.items():
+    for rule, rtype in zip(rules, ruletypes):
         if substr in rtype:
             matching.append((rule, rtype))
     return matching
+
+def simplify_ruletype(ruletype: str) -> str:
+    if "Agrega" in ruletype:
+        return "Agrega"
+    return ruletype
+
+def build_df_rules_from_path(path: str) -> pd.DataFrame:
+    print("Loading rules file from:", path)
+    df_rules = pd.read_csv(path)
+
+    rules = [ rg.Rule(rule_str) for rule_str in list(df_rules["rules"])]
+
+    df_rules["consequent"] = [ rule.consequent for rule in rules ]
+    df_rules["rule_size"] = [ 1 + len(rule.antecedent) for rule in rules ]
+
+    print("Classifying rules...")
+    ruletypes = get_ruletypes(rules)
+    ruletypes_simple = list(map(simplify_ruletype, ruletypes))
+
+    df_rules["ruletype"] = ruletypes
+    df_rules["ruletype_simple"] = ruletypes_simple
+
+    return df_rules
 
 def build_info_rules_for(family, min_len, mr_type, min_support, min_confidence):
     print(f"build_info_rules_for({family}, {min_len}, {mr_type}, {min_support}, {min_confidence})")
@@ -103,7 +125,7 @@ def build_info_rules_for(family, min_len, mr_type, min_support, min_confidence):
     info_rules["path_rules"] = f"output/rules/{family}_{min_len}_{mr_type}_s{min_support}_c{min_confidence}.csv"
 
     print("Reading rules file...")
-    df_rules = pd.read_csv(info_rules["path_rules"])
+    df_rules = build_df_rules_from_path(info_rules["path_rules"])
 
     print("Building rules from file...")
     rules = build_rule_list_from_df(df_rules)
@@ -115,25 +137,31 @@ def build_info_rules_for(family, min_len, mr_type, min_support, min_confidence):
     info_rules["rules_unique_consecuents"] = len(set(consequents(rules)))
 
     print("Classifying rules...")
-    ruletypes_by_rule = get_ruletypes(list(df_rules["rules"]))
-    info_rules["rules_type_add"] = len(ruletypes_with("Agrega", ruletypes_by_rule))
-    info_rules["rules_type_overlap"] = len(ruletypes_with("Overlap", ruletypes_by_rule))
-    info_rules["rules_type_n/a"] = len(ruletypes_with("N/A", ruletypes_by_rule))
+    rules_str = list(df_rules["rules"])
+    ruletypes = list(df_rules["ruletype"])
 
-    # for rt in ruletypes_with("Agrega", ruletypes_by_rule):
+    info_rules["rules_type_add"] = len(ruletypes_with("Agrega", rules_str, ruletypes))
+    info_rules["rules_type_overlap"] = len(ruletypes_with("Overlap", rules, ruletypes))
+    info_rules["rules_type_n/a"] = len(ruletypes_with("N/A", rules_str, rules_str))
+
+    print(df_rules.head())
+
+    # for rt in ruletypes_with("N/A", rules, ruletypes):
     #     print(rt)
 
-    print("Reading transactions file...")
-    transactions = transactions_from_path(info_rules["path_transactions"])
-    print("Calculating transactions stats...")
-    txs_items = all_mrs_from_transactions(transactions)
+    # print("Reading transactions file...")
+    # transactions = transactions_from_path(info_rules["path_transactions"])
+    # print("Calculating transactions stats...")
+    # txs_items = all_mrs_from_transactions(transactions)
 
-    info_rules["txs_amount"] = len(transactions)
-    info_rules["txs_items"] = len(txs_items)
-    info_rules["txs_unique_items"] = len(set(txs_items))
+    # info_rules["txs_amount"] = len(transactions)
+    # info_rules["txs_items"] = len(txs_items)
+    # info_rules["txs_unique_items"] = len(set(txs_items))
 
     return info_rules
 
+
+import sys
 
 if __name__ == '__main__':
 
@@ -142,6 +170,11 @@ if __name__ == '__main__':
     mr_type = "ALL_sub"
     min_support = "0.025"
     min_confidence = "0.9"
+
+    if len(sys.argv) > 1:
+        print("sys.argv", sys.argv)
+        assert(len(sys.argv) == 6)
+        family, min_len, mr_type, min_support, min_confidence = sys.argv[1:]
 
     info_rules = build_info_rules_for(family, min_len, mr_type, min_support, min_confidence)
 
