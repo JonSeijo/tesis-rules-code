9532f144d35404d13f0441a21dce224fd1e4dcea
jonno <jon.seijo@gmail.com>
2022-11-22 17:55:57 -0300

    Add timeout parameter in multiples rule generation

--- generate_rules_multiples.py
+++ generate_rules_multiples.py
@@ -9,14 +9,23 @@ import shlex
 # min_lens = ["4", "5", "6", "7", "8"]
 # supports = ["0.015", "0.020", "0.025", "0.030"]
 # confidences = ["0.9"]
+# max_time="30"
 
-family = "TPR1"
+# family = "TPR1"
+# clean_mode = "min"
+# mr_types = ["ALL", "NE", "NN", "SMR"]
+# min_lens = ["4"]
+# supports = ["0.015", "0.020", "0.025", "0.030"]
+# confidences = ["0.9"]
+# max_time="30"
+
+family = "LRR1"
 clean_mode = "min"
 mr_types = ["ALL", "NE", "NN", "SMR"]
 min_lens = ["4"]
 supports = ["0.015", "0.020", "0.025", "0.030"]
 confidences = ["0.9"]
-
+max_time="60"
 
 
 for min_len in min_lens:
@@ -27,7 +36,7 @@ for min_len in min_lens:
                 transaction_name = f"{family}_len{min_len}_{mr_type}_{clean_mode}"
 
 
-                bash_command = f"./generate_rules.r --transactions_name={transaction_name} --min_support={support} --min_confidence={confidence}"
+                bash_command = f"./generate_rules.r --transactions_name={transaction_name} --min_support={support} --min_confidence={confidence} --max_time={max_time}"
                 print(f"Running: {bash_command}")
 
                 # https://stackoverflow.com/questions/4256107/running-bash-commands-in-python
